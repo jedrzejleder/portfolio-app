13936452dd9d0874ee2aa033a4e0896e04370779
jedrzejleder <jedrzej.leder@billennium.com>
2021-07-07 15:47:06 +0200

    refactor of navbar and files

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import Hobby from '../src/typescript/Hobby.class'
 import isHobby from '../src/typescript/IsHobby.interface'
 import Navbar from './typescript/navbar';
 
+document.querySelector('nav').appendChild(Navbar());
+
 const myHobbies = [];
 myHobbies.push(new Hobby('Technologia', 'Fascynuje mnie nowoczesna technologia i jej dynamiczny rozwój. W wolnym czasie śledzę nowinki ze świata IT, motoryzacji czy Game Dev\'u', 'https://cdn.pixabay.com/photo/2017/10/05/08/20/technology-2818664_960_720.jpg'));
 myHobbies.push(new Hobby('Sport', 'Fascynuje mnie nowoczesna technologia i jej dynamiczny rozwój. W wolnym czasie śledzę nowinki ze świata IT, motoryzacji czy Game Dev\'u', 'https://cdn.pixabay.com/photo/2014/10/14/20/24/soccer-488700_960_720.jpg'));
--- src/typescript/navbar.ts
+++ src/typescript/navbar.ts
@@ -1,18 +1,14 @@
 const Navbar = () => {
-    const nav = document.createElement('nav');
+    const nav = document.createElement('ul');
     nav.innerHTML = `
-        <nav>
-            <ul>
-                <div>
-                    <li><a href="index.html" id="home"><i class="fas fa-user-astronaut">It's me!</a></li>
-                </div>
-                <div>
-                    <li><a href="aboutMe.html">O mnie</a></li>
-                    <li><a href="hobby.html">Hobby</a></li>
-                    <li><a href="contact.html">Kontakt</a></li>
-                </div>
-            </ul>
-        </nav>
+            <div>
+                <li><a href="index.html" id="home"><i id="logo" class="fas fa-user-astronaut"></i>It's me!</a></li>
+            </div>
+            <div>
+                <li><a href="aboutMe.html">O mnie</a></li>
+                <li><a href="hobby.html">Hobby</a></li>
+                <li><a href="contact.html">Kontakt</a></li>
+            </div>
     `;
 
     return nav;

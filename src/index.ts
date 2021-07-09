f7b2c7dcf1768c8064b344994d4d85eda11fa82d
jedrzejleder <jedrzejleder@gmail.com>
2021-07-09 09:35:57 +0200

    BEM implemented to SCSS

--- src/index.ts
+++ src/index.ts
@@ -23,15 +23,15 @@ myHobbies.push(new Hobby('Technologia', 'Fascynuje mnie nowoczesna technologia i
 myHobbies.push(new Hobby('Sport', 'Fascynuje mnie nowoczesna technologia i jej dynamiczny rozwój. W wolnym czasie śledzę nowinki ze świata IT, motoryzacji czy Game Dev\'u', 'https://cdn.pixabay.com/photo/2014/10/14/20/24/soccer-488700_960_720.jpg'));
 myHobbies.push(new Hobby('Kitesurfing', 'Fascynuje mnie nowoczesna technologia i jej dynamiczny rozwój. W wolnym czasie śledzę nowinki ze świata IT, motoryzacji czy Game Dev\'u', 'https://cdn.pixabay.com/photo/2020/08/18/06/06/extreme-5497194_960_720.jpg'));
 
-const slides = document.querySelector('.slides');
+const slides = document.querySelector('.hobbies__slides');
 
 const createHobbySlide = ({ name, desc, pic }: isHobby) => {
     const slide = document.createElement('div');
-    slide.classList.add('hobby');
+    slide.classList.add('hobbies__slides__hobby');
     slide.style.backgroundImage = `url(${pic})`
 
     slide.innerHTML = `
-        <div class="content">
+        <div class="hobbies__slides__hobby__content">
             <h2>${name}</h2>
             <p>${desc}</p>
         </div>
@@ -56,8 +56,8 @@ myHobbiesSlides.map((slide) => slides.appendChild(slide));
 
 
 ///////////////////////////////////////////////////////////////////////////
-const slide = <HTMLElement>document.querySelector('.slides');
-const hobbies = Array.from(document.querySelectorAll('.hobby'));
+const slide = <HTMLElement>document.querySelector('.hobbies__slides');
+const hobbies = Array.from(document.querySelectorAll('.hobbies__slides__hobby'));
 const prevBtn = document.querySelector('#prevBtn');
 const nextBtn = document.querySelector('#nextBtn');
 
